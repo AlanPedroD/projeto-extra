12de7fdec92bea43f1d75d1785a334924150405c
Alan Dias <alandiaslp@hotmail.com>
2024-10-29 17:00:02 -0300

    editando o js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,10 @@ let botao = document.querySelector('#botao-dark');
 
 botao.addEventListener('click', function() {
     botao.classList.toggle('light-mode');
+
+    if (botao.classList.contains('light-mode')) {
+        botao.textContent = 'Light';
+    } else {
+        botao.textContent = 'Dark';
+    }
 });
