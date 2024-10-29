c25c2cdcd0d5a0d77086f5d5733ccc02372a884a
Alan Dias <alandiaslp@hotmail.com>
2024-10-29 11:45:23 -0300

    js editado

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
 let botao = document.querySelector('#botao-dark');
 
-botao.addEventListener('click', function(){
-    if(botao.textContent === 'Dark'){
-        botao.textContent = 'Light';
-        botao.style.backgroundColor = "white";
-        botao.style.color = "black";
-    }else{
-        botao.textContent = 'Dark';
-        botao.style.backgroundColor = "black";
-        botao.style.color = "white";
-    }
-})
+botao.addEventListener('click', function() {
+    botao.classList.toggle('light-mode');
+});
