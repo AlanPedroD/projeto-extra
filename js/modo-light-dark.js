592f1e5eed467674567723dea209481eecbad772
Alan Dias <alandiaslp@hotmail.com>
2024-11-18 17:26:45 -0300

    excluindo fonte nao utilizada

--- js/modo-light-dark.js
+++ js/modo-light-dark.js
@@ -1,34 +1,34 @@
-// const botaoMode = document.querySelector('#botao-dark');
-// const elementosParaAlterar = [
-//     document.querySelector('header'),
-//     document.querySelector('body'),
-//     document.querySelector('.banner'),
-//     document.querySelector('.button'),
-//     document.querySelector('header ul')
-// ];
-// const logo = document.querySelector('.logo img');
+const botaoMode = document.querySelector('#botao-dark');
+const elementosParaAlterar = [
+    document.querySelector('header'),
+    document.querySelector('body'),
+    document.querySelector('.banner'),
+    document.querySelector('.button'),
+    document.querySelector('header ul')
+];
+const logo = document.querySelector('.logo img');
 
-// function trocarLogo(lightModeAtivo) {
-//     logo.src = lightModeAtivo ? "../image/logo-dark.png" : "../image/logo-claro (3).png";
-// }
+function trocarLogo(lightModeAtivo) {
+    logo.src = lightModeAtivo ? "../image/logo-dark.png" : "../image/logo-claro (3).png";
+}
 
-// botaoMode.addEventListener('click', function() {
-//     const lightModeAtivo = botaoMode.classList.toggle('light-mode');
+botaoMode.addEventListener('click', function() {
+    const lightModeAtivo = botaoMode.classList.toggle('light-mode');
     
-//     botaoMode.textContent = lightModeAtivo ? 'Light' : 'Dark';
+    botaoMode.textContent = lightModeAtivo ? 'Light' : 'Dark';
     
-//     trocarLogo(lightModeAtivo);
+    trocarLogo(lightModeAtivo);
     
-//     for (const elemento of elementosParaAlterar) {
-//         elemento.classList.toggle('light-mode');
-//     }
-// });
+    for (const elemento of elementosParaAlterar) {
+        elemento.classList.toggle('light-mode');
+    }
+});
 
-// let botaoExplorar = document.querySelector('.button');
-// let secaoCursos = document.querySelector('.cursos');
+let botaoExplorar = document.querySelector('.button');
+let secaoCursos = document.querySelector('.cursos');
 
-// botaoExplorar.addEventListener('click', function(){
-//     secaoCursos.scrollIntoView({
-//         behavior: 'smooth'
-//     })
-// })
+botaoExplorar.addEventListener('click', function(){
+    secaoCursos.scrollIntoView({
+        behavior: 'smooth'
+    })
+})
